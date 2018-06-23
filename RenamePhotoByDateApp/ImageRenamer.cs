21dde1cb874a67da6a804ad073fe663100d6f965
Павел Сидоров <pasha.sidorov@gmail.com>
2018-06-23 17:54:20 +0300

    Add creation time analyzer, improve performance

--- RenamePhotoByDateApp/ImageRenamer.cs
+++ RenamePhotoByDateApp/ImageRenamer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetadataExtractor;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -35,14 +36,14 @@ namespace RenamePhotoByDateApp
         {
             string newDir = FolderPath + "/" + SortedFolderName;
             int dirIterator = 1;
-            if (!Directory.Exists(newDir))
-                Directory.CreateDirectory(newDir);
+            if (!System.IO.Directory.Exists(newDir))
+                System.IO.Directory.CreateDirectory(newDir);
             else
             {
-                while (Directory.Exists($"{newDir}{dirIterator}"))
+                while (System.IO.Directory.Exists($"{newDir}{dirIterator}"))
                     dirIterator++;
                 newDir += dirIterator.ToString();
-                Directory.CreateDirectory(newDir);
+                System.IO.Directory.CreateDirectory(newDir);
             }
             int counter = 1;
             string format = new string('0', photos.Count.ToString().Length);
@@ -67,12 +68,12 @@ namespace RenamePhotoByDateApp
         private List<Photo> GetFilesInFolder(string path = null)
         {
             List<Photo> photos = new List<Photo>();
-            foreach (var dir in Directory.EnumerateDirectories(path??FolderPath))
+            foreach (var dir in System.IO.Directory.EnumerateDirectories(path??FolderPath))
             {
                 if (!dir.Substring(dir.LastIndexOfAny(new char[] { '/', '\\' }) + 1).StartsWith(SortedFolderName))
                     photos.AddRange(GetFilesInFolder(dir));
             }
-            foreach (var file in Directory.EnumerateFiles(path??FolderPath))
+            foreach (var file in System.IO.Directory.EnumerateFiles(path??FolderPath))
             {
                 photos.Add(new Photo(file, GetCreatedOnOfFile(file)));
             }
@@ -97,26 +98,56 @@ namespace RenamePhotoByDateApp
             private const int CreatedOnPropertyId = 36867;
             public DateTime GetDateTakenFromImage(string path)
             {
+                IEnumerable<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(path);
+                foreach (var directory in directories)
+                {
+                    if (directory.Name != "Exif IFD0")
+                        continue;
+                    foreach (var tag in directory.Tags)
+                    {
+                        if (tag.Name != "Date/Time")
+                            continue;
+                        return DateTime.Parse(GetStringCorrectDate(tag.Description));
+                    }
+                }
+
                 try
                 {
-                    using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-                    {
-                        using (Image photo = Image.FromStream(fs, false, false))
+                    string lowP = path.ToLower();
+                    if (lowP.EndsWith(".jpg") || lowP.EndsWith(".jpeg") || lowP.EndsWith(".bmp") || lowP.EndsWith(".png"))
+                        using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
                         {
-                            if (photo.PropertyIdList.Contains(CreatedOnPropertyId))
+                            using (Image photo = Image.FromStream(fs, false, false))
                             {
-                                PropertyItem propItem = photo.GetPropertyItem(CreatedOnPropertyId);
-                                string dateTaken = _r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
-                                return DateTime.Parse(dateTaken);
+                                if (photo.PropertyIdList.Contains(CreatedOnPropertyId))
+                                {
+                                    PropertyItem propItem = photo.GetPropertyItem(CreatedOnPropertyId);
+                                    string dateTaken = _r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                                    return DateTime.Parse(dateTaken);
+                                }
                             }
                         }
-                    }
                 }
                 catch { }
 
                 DateTime creation = File.GetCreationTime(path);
                 DateTime modification = File.GetLastWriteTime(path);
                 return modification < creation ? modification : creation;
+            }
+
+            private string GetStringCorrectDate(string value)
+            {
+                var parts = value.Split(' ');
+                if (parts.Length==2)
+                {
+                    if (parts[0].Length > parts[1].Length)
+                    {
+                        return parts[0].Replace(':', '.') + ' ' + parts[1];
+                    }
+                    else
+                        return parts[0] + ' ' + parts[1].Replace(':', '.');
+                }
+                throw new NotImplementedException();
             }
         }
 
